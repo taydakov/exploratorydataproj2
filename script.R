db97d3c6b2be58cd8a7e265109316274b7260bee
Lev Taydakov <lev.taydakov@gmail.com>
2014-10-23 23:34:28 -0700

    question #5 completed

--- script.R
+++ script.R
@@ -27,14 +27,32 @@ q3 <- sqldf('select year, type, sum(Emissions) as total
 qplot(year, total, data = q3, facets = . ~ type, geom = "line", main = "Emission of PM2.5 in Baltimore, MD by emission type", xlab = "Year", ylab = "Emission of PM2.5")
 
 # Question #4
-# I take SCCs where EI.Sector contains COAL. There are [13, 18, 23] SCCs.
+# I take SCCs where EI.Sector contains COAL. There are [13, 18, 23] factor levels of SCC$EI.Sector
 # It includes:
-# "Fuel Comb - Comm/Institutional - Coal", 
-# "Fuel Comb - Electric Generation - Coal",
-# "Fuel Comb - Industrial Boilers, ICEs - Coal"
+# [13] "Fuel Comb - Comm/Institutional - Coal", 
+# [18] "Fuel Comb - Electric Generation - Coal",
+# [23] "Fuel Comb - Industrial Boilers, ICEs - Coal"
 q4 <- sqldf('select year, sum(NEI.Emissions) as total 
              from NEI
              join SCC on SCC.SCC = NEI.SCC
              where SCC.EISector in (13, 18, 23)
              group by year')
 qplot(year, total, data = q4, geom = "line", ylim = c(0, max(q4$total)), main = "Emission of coal combustion-related sources", xlab = "Year", ylab = "Emission of PM2.5")
+
+# Question #5
+# I take SCCs where motor vehicles are. There are [43, 44, 45, 49, 50, 51, 52] factor levels of SCC$EI.Sector
+# It includes:
+# [43] "Mobile - Aircraft"
+# [44] "Mobile - Commercial Marine Vessels"
+# [45] "Mobile - Locomotives"
+# [49] "Mobile - On-Road Diesel Heavy Duty Vehicles"
+# [50] "Mobile - On-Road Diesel Light Duty Vehicles"
+# [51] "Mobile - On-Road Gasoline Heavy Duty Vehicles"
+# [52] "Mobile - On-Road Gasoline Light Duty Vehicles"
+q5 <- sqldf('select year, sum(NEI.Emissions) as total 
+             from NEI
+             join SCC on SCC.SCC = NEI.SCC
+             where SCC.EISector in (43, 44, 45, 49, 50, 51, 52)
+             and NEI.fips = "24510"
+             group by year')
+qplot(year, total, data = q5, geom = "line", ylim = c(0, max(q5$total)), main = "Emission from motor vehicle sources in Baltimore, MD", xlab = "Year", ylab = "Emission of PM2.5")
