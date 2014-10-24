78e7311abefd6c1bece659ec8969dc76488cd45e
Lev Taydakov <lev.taydakov@gmail.com>
2014-10-23 19:36:33 -0700

    question #2 completed

--- script.R
+++ script.R
@@ -3,5 +3,14 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 # Question #1
-q1 <- sqldf('select year, sum(Emissions) as total from NEI group by year')
-plot(q1$year, q1$total, type="l", ylim = c(0, max(q1$total)), main = "Total emission of PM2.5 each year", xlab = "Year", ylab = "Total emission")
+q1 <- sqldf('select year, sum(Emissions) as total 
+             from NEI 
+             group by year')
+plot(q1$year, q1$total, type="l", ylim = c(0, max(q1$total)), main = "Total emission of PM2.5 each year", xlab = "Year", ylab = "Total emission of PM2.5")
+
+# Question #2
+q2 <- sqldf('select year, sum(Emissions) as total 
+             from NEI 
+             where fips = "24510"
+             group by year')
+plot(q2$year, q2$total, type="l", ylim = c(0, max(q2$total)), main = "Emission of PM2.5 in Baltimore, MD each year", xlab = "Year", ylab = "Emission of PM2.5")
